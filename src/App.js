cca64d06a9a6e8790160d01ccae7d66101f1dad1
Ilavbare Adeola <raisingsrs@gmail.com>
2023-09-15 19:12:55 +0100

    Axios Integration

--- src/App.js
+++ src/App.js
@@ -1,40 +1,67 @@
-
+import React, {useState} from "react";
 import './App.css';
 import SearchEngine from './SearchEngine';
+import axios from "axios";
+import Forecast from "./Forecast";
 
-function App() {
+function App(props) {
+
+  const [temp, setTemp] = useState("null");
+  const [description, setDescription] = useState(" ");
+  const [humidity, setHumidity] = useState("");
+  const [wind, setWind] = useState("");
+  const [cityName, changeCityName] = useState("");
+
+  
+  function getWeatherUpdate(response){
+    console.log(response.data);
+    setTemp (Math.round(response.data.main.temp));
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    changeCityName(response.data.name);
+
+   
+  }
+
+let apiKey = "1d038ee28ef2727a9f0310860ac10ae9";
+let apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+axios.get(apiLink).then(getWeatherUpdate);
+
+
   return (
     <div className="container App mt-5 ">
-      <h1>Weather App</h1>
+    
       <SearchEngine />
       
       <div className="container">
       <div className='row'>
-        <div className='col-sm-4'><img src="https://openweathermap.org/img/wn/04n@2x.png" alt="overcast clouds" id="icon" className='img-icon'/><strong className='temp'>24</strong><span className='degree'>°C</span></div>
-        <div className='col-sm-8'><h2>Lagos</h2></div>
+        <div className='col-sm-4'><img src="https://openweathermap.org/img/wn/04n@2x.png" alt="overcast clouds" id="icon" className='img-icon img-fluid'/><strong className="temp">{temp}</strong><span className='degree'>°C</span></div>
+        <div className='col-sm-8'><h2>{cityName}</h2></div>
       </div>
         <div className='row'>
     <div className='col-3'>Updated On: Wed 23:30</div>
-    <div className='col-3'>Overcast Clouds</div>
-    <div className='col-3'>Humidity: 94%</div>
-    <div className='col-3'>Wind: 2km/H</div>
+    <div className='col-3'>{description}</div>
+    <div className='col-3'>Humidity: {humidity}%</div>
+    <div className='col-3'>Wind: {wind}km/h</div>
    </div>
 
-     <div className='row mt-5'>
-    <div className='col-3'>Mon</div>
-    <div className='col-2'>Tues</div>
-    <div className='col-2'>Wed</div>
-    <div className='col-2'>Thurs</div>
-    <div className='col-3'>Fri</div>
-   </div>
+     <Forecast />
+   <footer>This Project Was Coded by <a href ="https://github.com/Adeolade1st/react-weather-project">Ilavbare Adeola</a>  and Hosted on render.</footer>
    </div>
 
    
- <footer>This Project Was Coded by <a href ="https://github.com/Adeolade1st/react-weather-project">Ilavbare Adeola</a>  and Hosted on render.</footer>
+ 
     </div>
-    
+
+   
 
   );
+
+  
+
+
+  
   
 }
 
---
+++ src/Forecast.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Forecast(){
+    return(
+        <div>
+
+        <div className="container">
+             <div className='row mt-4'>
+    <div className='col-2'>Mon <div><img src="https://openweathermap.org/img/wn/04n@2x.png" alt="overcast clouds" id="icon" className='img-icon img-fluid' /> <div> <strong>30°</strong> 18°</div></div></div>
+    <div className='col-2'>Tues <div><img src="https://openweathermap.org/img/wn/04n@2x.png" alt="overcast clouds" id="icon" className='img-icon img-fluid'/> <div> <strong>30°</strong> 18°</div></div></div>
+    <div className='col-2'>Wed <div><img src="https://openweathermap.org/img/wn/04n@2x.png" alt="overcast clouds" id="icon" className='img-icon img-fluid'/> <div> <strong>30°</strong> 18°</div></div></div>
+    <div className='col-2'>Thurs <div><img src="https://openweathermap.org/img/wn/04n@2x.png" alt="overcast clouds" id="icon" className='img-icon img-fluid'/> <div> <strong>30°</strong> 18°</div></div></div>
+    <div className='col-2'>Fri <div><img src="https://openweathermap.org/img/wn/04n@2x.png" alt="overcast clouds" id="icon" className='img-icon img-fluid'/> <div> <strong>30°</strong> 18°</div></div></div>
+    <div className='col-2'>Sat <div><img src="https://openweathermap.org/img/wn/04n@2x.png" alt="overcast clouds" id="icon" className='img-icon img-fluid'/> <div> <strong>30°</strong> 18°</div></div></div>
+   </div>
+        </div>
+
+
+        </div>
+    );
+}
